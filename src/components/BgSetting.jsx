af5c7eccebec51650eca5579e425b2aab47520f4
Yute Lilitprapun <yudthsoponvit@gmail.com>
2026-05-31 08:44:19 -0400

    added new bg

--- src/components/BgSetting.jsx
+++ src/components/BgSetting.jsx
@@ -8,14 +8,14 @@ import bgPic from '../assets/bgpc.png'
 import bg0 from '../assets/bg0.png'
 import bg1 from '../assets/bg1.png'
 import bg2 from '../assets/bg2.jpg'
-import bg3 from '../assets/bg3.jpg'
+import bg3 from '../assets/nature.gif'
 import bg5 from '../assets/bg5.png'
 import bg6 from '../assets/bg6.jpg'
 import bg7 from '../assets/bg7.png'
 import bg8 from '../assets/bg8.png'
 import bg9 from '../assets/bg9.jpg'
 import bg10 from '../assets/live_grey.gif'
-import bg11 from '../assets/live_light_grey.gif'
+import bg11 from '../assets/swordBG.gif'
 import bg12 from '../assets/Computer_bg.png'
 import eff1 from '../assets/noise.png'
 import eff2 from '../assets/glitch2.jpg'
@@ -85,14 +85,14 @@ function BgSetting() {
         { value: 13, label: 'Choose your favorite color', color: userPickedColor, image: userPickedColor, barColor: userPickedColor},
         { value: 2, label: 'Purple Summer', color: '#3F4565', image: bg1, barColor: '#3F4565'},
         { value: 3, label: 'Matt Blue', color: '#456EA6', image: bg2, barColor: '#456EA6'},
-        { value: 4, label: 'Matt Green', color: '#008081', image: bg3, barColor: '#008081'},
+        { value: 4, label: 'Nature', color: '#008081', image: bg3, barColor: '#008081'},
         { value: 6, label: 'Blue Sky', color: '#4B6894', image: bg5, barColor: '#4B6894'},
         { value: 7, label: 'Dark Tone', color: '#313439', image: bg6, barColor: '#313439'},
         { value: 8, label: 'Light Pink', color: '#f3aac0', image: bg7, barColor: '#1c1719'},
         { value: 9, label: 'Deep Ocean', color: '#3F4565', image: bg8, barColor: '#3F4565'},
         { value: 10, label: 'Purple Blue', color: '#354092', image: bg9, barColor: '#354092'},
         { value: 11, label: 'Live Wavy Grey', color: '#3C3C3C', image: bg10, barColor: '#3C3C3C'},
-        { value: 12, label: 'Live Wavy Light Grey', color: '#828890', image: bg11, barColor: '#4a4a4a'},
+        { value: 12, label: 'Abandoned Yard', color: '#828890', image: bg11, barColor: '#4a4a4a'},
         { value: 14, label: 'Computer Disk', color: '#098684', image: bg12, barColor: '#14045c'},
       ];
       
